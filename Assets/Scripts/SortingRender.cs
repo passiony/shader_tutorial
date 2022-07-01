7786dab03a5c7d5104091edb35e0db339ccaf60a
passion <yupu@gillar.com>
2022-07-01 12:22:51 +0800

    整理资源

---
+++ Assets/Scripts/SortingRender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteAlways]
+public class SortingRender : MonoBehaviour
+{
+    private Renderer myRender;
+
+    public string sortingLayerName = "Default"; //方法前的初始化
+    public int orderInLayer = 0;
+
+    private void Awake()
+    {
+        myRender = this.GetComponent<Renderer>();
+    }
+
+    void SetSortingLayer()
+    {
+        if (sortingLayerName != string.Empty)
+        {
+            myRender.sortingLayerName = sortingLayerName;
+            myRender.sortingOrder = orderInLayer;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        SetSortingLayer();
+    }
+#endif
+}
